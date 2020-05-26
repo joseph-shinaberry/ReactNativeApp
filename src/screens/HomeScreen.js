e1d34a274cf03bf129eaa4734aa8010bc5424eac
Joseph Shinaberry <joseph.shinaberry@agilesof.com>
2020-05-26 13:41:07 -0400

    images added to app:

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = (props) => {
-  console.log(props);
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.textStyle}>HomeScreen</Text>
       <Button
-        onPress={() => props.navigation.navigate('Components')}
+        onPress={() => navigation.navigate('Components')}
         title='Go to components Demo' />
-        <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
-          <Text>Got To List Demo</Text>
-        </TouchableOpacity>
+      <Button
+        onPress={() => navigation.navigate('List')}
+        title='Go to List Demo' />
+        <Button
+        onPress={() => navigation.navigate('Images')}
+        title='Go to Images Demo' />
     </View>
   );
 };
---
+++ src/screens/ImageScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import ImageDetail from '../components/ImageDetails';
+
+const ImageScreen = () => {
+    return <View>
+        <ImageDetail title='Forest' imageSource={require('../../assets/forest.jpg')} score={5}/>
+        <ImageDetail title='Beach' imageSource={require('../../assets/beach.jpg')} score={6}/>
+        <ImageDetail title='Mountain' imageSource={require('../../assets/mountain.jpg')} score={7}/>
+    </View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageScreen;
