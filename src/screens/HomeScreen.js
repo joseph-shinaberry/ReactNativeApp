04f7046697a3040779c48343e65c198c3090ce7c
Joseph Shinaberry <joseph.shinaberry@agilesof.com>
2020-05-26 10:55:08 -0400

    added button with navigation, added touchopacity with navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,19 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.textStyle}>HomeScreen</Text>;
+const HomeScreen = (props) => {
+  console.log(props);
+  return (
+    <View>
+      <Text style={styles.textStyle}>HomeScreen</Text>
+      <Button
+        onPress={() => props.navigation.navigate('Components')}
+        title='Go to components Demo' />
+        <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
+          <Text>Got To List Demo</Text>
+        </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
